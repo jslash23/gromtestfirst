06634bc6de5c369bdea5222474cad6506a63de94
slash22 <slash22@bigmir.net>
2018-11-01 22:45:15 +0200

    commit 7

---
+++ src/L33_InputOutputStreams/Exemples/HW_33/Demo.java
@@ -0,0 +1,9 @@
+package L33_InputOutputStreams.Exemples.HW_33;
+
+import java.io.IOException;
+
+public class Demo {
+    public static void main(String[] args) throws IOException {
+        WrToFileFromCons.writeToFileFromConsol("C:/Users/slash22/Desktop/test2.txt");
+    }
+}
---
+++ src/L33_InputOutputStreams/Exemples/HW_33/WrToFileFromCons.java
@@ -0,0 +1,70 @@
+package L33_InputOutputStreams.Exemples.HW_33;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+import org.apache.commons.io.IOUtils;
+
+
+public class WrToFileFromCons {
+
+    public static void writeToFileFromConsol(String path) throws IOException{
+
+        BufferedReader bufread = new BufferedReader(new  InputStreamReader(System.in));//объект который читает целыми строками
+        System.out.println("Enter file content to write in the file");
+
+        String text = bufread.readLine();
+
+        try{
+            WRText(text,path);
+        }
+        catch (FileNotFoundException e) {
+            System.err.println("File with path " + path + " not found");
+            return;
+        }
+        finally {
+            bufread.close();
+            //must close bufer of writer
+        }
+    }
+
+    public static void WRText(String text,String path)throws IOException{
+        FileWriter writer = null;
+        BufferedWriter bfWriter = null;
+
+            while ((text) != null){
+               try {if (TestWR(text)){
+                   writer = new FileWriter(path,true);
+                   bfWriter = new BufferedWriter(writer);
+
+                   bfWriter.append(text);
+                   bfWriter.append("\n");
+                   bfWriter.append("3333333333333");
+                   System.out.println("new text was writen");
+               }
+            }catch (IOException e){
+                   System.err.println("Can't write to file with path " + path );
+               }
+               break;
+            //System.out.println("Please enter command WR when you finished enter you text");
+        }
+
+        IOUtils.closeQuietly(writer);
+        IOUtils.closeQuietly(bfWriter);
+    }
+
+    private static boolean TestWR(String reader){
+
+        String[] arrtext = reader.split(" ");
+        for (String art : arrtext) {
+            if (art.equalsIgnoreCase("wr")){
+                return true;
+            }
+        }
+        return false;
+    }
+}
