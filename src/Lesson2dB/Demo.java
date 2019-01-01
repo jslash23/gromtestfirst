a84398779f6a45df5aa828f75d229c1528d426cd
slash22 <slash22@bigmir.net>
2019-01-01 14:23:54 +0200

    commit 11dataBase

---
+++ src/Lesson2dB/Demo.java
@@ -0,0 +1,20 @@
+package Lesson2dB;
+
+/**
+ * Created by slash22 on 30.12.2018.
+ */
+public class Demo {
+    public static void main(String[] args) {
+        //JAVA
+        //1. create class +
+        //2. create object with some values
+        //3. using it
+
+        //DB
+        //1. create table with columns -
+        //2. insert some data to the table
+        //3. using it
+
+
+    }
+}
---
+++ src/Lesson2dB/Order.java
@@ -0,0 +1,43 @@
+package Lesson2dB;
+
+import java.util.Date;
+
+/**
+ * Created by slash22 on 30.12.2018.
+ */
+public class Order {
+
+    private long id;
+    private String productName;
+    private int price;
+    private Date dateOrdered;
+    private Date dateConfirmed;
+
+    public Order(long id, String productName, int price, Date dateOrdered, Date dateConfirmed) {
+        this.id = id;
+        this.productName = productName;
+        this.price = price;
+        this.dateOrdered = dateOrdered;
+        this.dateConfirmed = dateConfirmed;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public Date getDateOrdered() {
+        return dateOrdered;
+    }
+
+    public Date getDateConfirmed() {
+        return dateConfirmed;
+    }
+}
